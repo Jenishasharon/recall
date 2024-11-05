82cbb44b3c4aeccf6211fe1e0c269ee2d10b6523
Jenisha Sharon <Jenisha Sharon@DESKTOP-08V9D63>
2024-11-05 12:23:24 +0530

    Encapsulation code

---
+++ Recall/src/main/java/recall_topics/Encap2.java
@@ -0,0 +1,15 @@
+package recall_topics;
+
+public class Encap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Encap1 obj=new Encap1();
+		obj.setName("jenisha");
+		obj.setAge(29);
+		System.out.println(obj.getName());
+		System.out.println(obj.getAge());
+		
+	}
+
+}
